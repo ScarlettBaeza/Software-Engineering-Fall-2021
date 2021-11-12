01d45c881d6030b7db0694c10f78b4b9c0e9c3ec
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2021-11-11 23:58:33 -0600

    removed secret

--- server/service.ts
+++ server/service.ts
@@ -5,7 +5,7 @@ export const collections: { Reservations?: mongoDB.Collection } = {}
 
 export async function connectDatabases() {
     console.log(dotenv.config());
-    const dbClient: mongoDB.MongoClient = new mongoDB.MongoClient("mongodb+srv://cooldevs:coolerdevs@cluster0.imgtn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    const dbClient: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.REACT_APP_DATABASE_CONNECTION_STRING!);
     await dbClient.connect();
     const database: mongoDB.Db =  dbClient.db(process.env.REACT_APP_DATABASE_NAME!);
     const reservationsCollection: mongoDB.Collection = database.collection(process.env.REACT_APP_RESERVATION_COLLECTION!);
