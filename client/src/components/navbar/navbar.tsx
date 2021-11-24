a26ae4421f55fe8bcadae1b60110164055631451
Scarlett Baeza <42827150+ScarlettBaeza@users.noreply.github.com>
2021-11-23 18:42:18 -0600

    more reformatting

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { Container, Nav } from 'react-bootstrap';
 import './navbar.css';
-import logo from '../../assets/images/logo.png';
 
 export const NavigationBar = () => {
 
@@ -10,17 +9,10 @@ export const NavigationBar = () => {
     return (
         <Navbar fixed = "top" expand="sm" variant="light" bg="light">
             <Container>
-                <Navbar.Brand href = "home">
-                    <img 
-                    src = {logo}
-                    alt = 'Logo'
-                    width = '200'
-                    height = '100'/>
-                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Nav className="me-auto">
-                <Nav.Link href="home">Home</Nav.Link>
-                    <Nav.Link href="resForm">Make A Reservation</Nav.Link>
+                    <Nav.Link href="home" className="links">Home</Nav.Link>
+                    <Nav.Link href="resForm" className="links">Make A Reservation</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
