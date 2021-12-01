99d3ed3d3d714ba53e37c1e3f91c48faf4982fd1
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2021-12-01 01:35:54 -0600

    rearrange arch

---
+++ server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { connectDatabases } from "./service";
+import { reservationsRouter } from "./routes/reservations.router";
+import { tablesRouter } from "./routes/tables.router";
+
+const app = express();
+const port = 8080; // default port to listen
+const path = require("path")
+
+connectDatabases().then(() => {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+    app.use("/reservation", reservationsRouter);
+    app.use("/table", tablesRouter);
+
+    app.use(express.static(path.join(__dirname, "client", "build")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+    
+    app.listen(port, () => {
+        console.log("Server started on localhost at port ", port);
+    });
+}).catch((error: Error)=>{
+    console.error("Couldn't connect to database ", error);
+});
